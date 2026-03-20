2032fa9e711deda79f4cec76b63c1f35e0768806
PierceWand <piercewand@gmail.com>
2026-03-20 09:42:41 -0500

    Update blinky.s

--- blinky/blinky.s
+++ blinky/blinky.s
@@ -6,17 +6,30 @@
 
 .data
 .balign 4
+
 gpio_oe:    .word   0xd0000024
 gpio_on:    .word   0xd0000014
-gpio_off:    .word   0xd0000018
+gpio_off:   .word   0xd0000018
+gpio_in:    .word   0xd0000004
 delay:  .word   1000
 .text
 .balign 4
 main:
+    @ enable GPIO on 4, 26, 27
     movs    r0, #LED
     bl gpio_init
     movs     r1, #1
 
+    movs r0, #27
+    bl gpio_init
+
+    movs    r0, #4
+    bl gpio_init
+
+    movs r0, #0
+    bl gpio_init
+
+    movs    r1, #1
     lsl r1, #LED
     ldr r2, =gpio_oe
     ldr r2, [r2]
@@ -35,4 +48,28 @@ main:
     ldr r2, =gpio_off
     ldr r2, [r2]
     str r1, [r2]
+
+loop:
+    ldr r1, =gpio_in
+    ldr r1, [r1]
+    ldr r0, [r1]
+    @ lsr r0, #4
+    lsl r0, #LED
+
+    @Put the button value in the on reg
+    ldr r1, =gpio_on
+    ldr r1, [r1]
+    str r0, [r1]
+
+    lsr r0, #LED
+    movs r1, #1
+    eor r0, r1
+    lsl r0, #LED
+
+    ldr r1, =gpio_off
+    ldr r1, [r1]
+    str r0, [r1]
+
+
+    b loop
 
