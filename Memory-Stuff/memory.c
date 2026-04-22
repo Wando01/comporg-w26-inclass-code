e80e20b055c4dc9a4f4de346f64330e77137dd75
PierceWand <piercewand@gmail.com>
2026-04-21 22:09:28 -0500

    Create memory.c

---
+++ Memory-Stuff/memory.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int arr1[4]; //arr1: .skip 16
+    float arr2[] = {3.14, .12, .23, 1.0642};
+    int allzeros[10] = {};
+    puts("Hello world!");
+    return 0;
+}
